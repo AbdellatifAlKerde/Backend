f92fece33b850e4baae38a59a63886b564d8a4ef
Aya Shahal <ayashahal055@gmail.com>
2023-01-24 10:01:24 +0200

    admin

---
+++ controllers/admincontrollers.js
@@ -0,0 +1,55 @@
+
+import Model from "../models/admin.js";
+import bcrypt from "bcrypt"
+class Controller {
+    // get all
+    getAll (req, res, next) {
+      Model.find({}, (err, response) =>{
+        if (err) return next(err);
+        res.status(200).send({ success: true, response });
+      })
+       
+    }
+   
+    //get by id
+    get(req, res, next) {
+      let { id } = req.params;
+      Model.findOne({ _id: id }, (err, response) => {
+          if (err) return next(err);
+          res.status(200).send({ success: true, response });
+      });
+    }
+
+// create
+async post(req,res,next) {
+  try{let body = req.body;
+  body.password = await bcrypt.hash(body.body, 10)
+  let newproduct = new Model(body) 
+  newproduct.save((error,response) => {
+    if (error) return res.status(500).send(error);
+    res.status(200).send({ success: true, response })});}catch(e){return res.status(500).send(e)}
+};
+
+// update
+put(req,res,next) {
+let { id }=req.params;
+Model.updateOne({_id:id}),(err,response) =>{
+  if (err) return next(err);
+  res.status(200).send({sucess: true ,response});
+}
+}
+// delete
+delete(req,res,next){
+let {id} =req.params;
+Model.findbyidAnddelete({_id:id}),(err,response) =>{
+  if(err) return next(err);
+   res.status(200).send({sucess: true ,response});
+}
+}
+}
+const controller = new Controller();
+
+export default controller;
+
+
+
